158e350301695ef0de85d6bbb76f5ec6a9b1055c
Rizky Eko Putra <rizky.putra@happyfresh.com>
2018-10-20 17:11:51 +0700

    Add tab

--- app/src/main/java/com/example/rizkyekoputra/footballmatchschedule/MainActivity.kt
+++ app/src/main/java/com/example/rizkyekoputra/footballmatchschedule/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.rizkyekoputra.footballmatchschedule
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fragmentAdapter = MyPagerAdapter(supportFragmentManager)
+        viewpager_main.adapter = fragmentAdapter
+
+        tabs_main.setupWithViewPager(viewpager_main)
     }
 }
